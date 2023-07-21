2d50ac9ad12926fa3165842c6a3b3d58ce751db0
CCeph <yousefelsisyy@gmail.com>
2023-07-21 17:38:39 -0300

    Add recursive fibonacci function

--- index.js
+++ index.js
@@ -19,3 +19,22 @@ function fibs(reptitions) {
 }
 
 console.log(fibs(8));
+
+function fibsRecursive(reptitions) {
+  if (reptitions <= 0) {
+    return [];
+  }
+  if (reptitions === 1) {
+    return [0];
+  }
+  if (reptitions === 2) {
+    return [0, 1];
+  }
+
+  const previousReptitions = fibsRecursive(reptitions - 1);
+  const lastNumber = previousReptitions[previousReptitions.length - 1];
+  const beforeLastNumber = previousReptitions[previousReptitions.length - 2];
+  return previousReptitions.concat(lastNumber + beforeLastNumber);
+}
+
+console.log(fibsRecursive(4));
