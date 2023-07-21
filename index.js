4f4e466b416208bb8aad3b57a6663a6dfc9e20b6
CCeph <yousefelsisyy@gmail.com>
2023-07-21 17:00:38 -0300

    Add iterative fibonacci function

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("ESLint & Prettier Template");
+function fibs(reptitions) {
+  if (reptitions <= 0) {
+    return [];
+  }
+  if (reptitions === 1) {
+    return [0];
+  }
+  if (reptitions === 2) {
+    return [0, 1];
+  }
+
+  const fibArr = [0, 1];
+  for (let i = reptitions - 2; i > 0; i -= 1) {
+    const lastNumber = fibArr[fibArr.length - 1];
+    const beforeLastNumber = fibArr[fibArr.length - 2];
+    fibArr.push(lastNumber + beforeLastNumber);
+  }
+  return fibArr;
+}
+
+console.log(fibs(8));
